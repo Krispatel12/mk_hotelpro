6a325c957c213fd7e84e752151b744bb14ffc9c0
Kris Patel <krispatel12@gmail.com>
2026-03-24 10:48:42 +0530

    Fix Pyre lint error

--- hotelpro/wsgi.py
+++ hotelpro/wsgi.py
@@ -5,12 +5,17 @@ import sys
 current_dir = os.path.dirname(os.path.abspath(__file__))
 
 # Change directory to the outer hotelpro folder 
-# This is critical so Django file paths (like db.sqlite3, templates, media) work
+# This is critical so Django file paths work
 os.chdir(current_dir)
 
 # Add it to the Python path so it can import the inner `hotelpro` and other apps
 if current_dir not in sys.path:
     sys.path.insert(0, current_dir)
+
+# VERY CRITICAL FIX: Gunicorn imported the *outer* hotelpro as `hotelpro`.
+# This shadows the *inner* hotelpro namespace holding settings.py!
+# We delete the cached outer module so Django successfully finds the inner one.
+sys.modules.pop('hotelpro', None)
 
 # Set the Django settings module
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hotelpro.settings')
